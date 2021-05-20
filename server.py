382b3cba38520d1b9e146b228e9563147dabfb0e
harshavardhan1729 <saiatthuluri@gmail.com>
2021-05-20 19:58:13 +0530

    server file

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+  
+import random
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+    return str(random.randint(0,100))
+
+  
+# main driver function
+if __name__ == '__main__':
+  
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
